8d3768501273da1900f03a3b8915ba6a4b0fef8f
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-20 13:10:33 +0300

    Add files via upload

--- f.js
+++ f.js
@@ -1,2 +1,15 @@
 
-document.querySelector('#description')
+var description = querySelector('#description');
+var incident = querySelector('#incident');
+var vehicles = querySelector("#vehicles");
+var casualties = querySelector('#casualties');
+var button = querySelector('#submit');
+
+button.addEventListener('click',()=>{
+
+    var obj = {
+        description:description.value,
+        incident:incident.value,
+    }
+
+})
