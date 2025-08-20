168d708d13bad5e851c98aa21e2ec67af855cd71
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-20 22:33:12 +0300

    Add files via upload

--- r.js
+++ r.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 /*saving the reference for the inputs */
 const form = document.getElementById('form');
@@ -19,4 +20,27 @@ const validateinputs = () =>{
     const passwordValue = password.value.trim();
 
 
+=======
+
+/*saving the reference for the inputs */
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateinputs();
+});
+
+
+const validateinputs = () =>{
+    /* removing the whitespaces */
+    const nameValue = name.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
+>>>>>>> 9b2f99b211aae695cdc127d25398d9b9b84f1c18
 };
