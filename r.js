4db10f06462489b2efbc2d62b242e4690d7bf9f1
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-20 10:47:28 +0300

    Add files via upload

--- r.js
+++ r.js
@@ -1,6 +1,6 @@
 
 /*saving the reference for the inputs */
-const form = document.getElementById("form");
+const form = document.getElementById('form');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
